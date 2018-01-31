4bd62396f5298d75a94a30a812641ca5c3fa3646
Tim Brunetto <tjbrunetto@gmail.com>
2018-01-31 15:30:17 -0500

    Using CrossPlatform Input Manager

---
+++ Test Project/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput; 
+
+public class Player : MonoBehaviour {
+
+
+	public float horizontalSpeed = 3.0f;
+	public float verticalSpeed = 3.0f;
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = horizontalSpeed * CrossPlatformInputManager.GetAxis("Horizontal");
+		float v = verticalSpeed * CrossPlatformInputManager.GetAxis("Vertical");
+			
+	}
+}
